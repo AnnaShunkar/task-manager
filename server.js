d72095ade839262146b253518e3943bce4a39ba1
AnnaShunkar <anna.shunkar4@gmail.com>
2026-03-22 20:14:30 +0200

    Creates endpoints for tasks

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import "./config/db.js";
 
 import authRouter from "./routes/authRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
 
 
 const app = express();
@@ -12,6 +13,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use('/api', authRouter);
+app.use('/api', taskRouter);
 
 app.listen(port, () => {
     console.log(
